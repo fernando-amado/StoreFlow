779d19f30fc836ede054ea93347878204005c3ff
Luis Fernando <Luis Fernando Amado>
2025-03-30 14:04:14 -0500

    instalacion capacitor para android y ios

---
+++ apps/mobile/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'storeflow.app',
+  appName: 'StoreFlow',
+  webDir: '../../dist/apps/browser/mobile',
+};
+
+export default config;
