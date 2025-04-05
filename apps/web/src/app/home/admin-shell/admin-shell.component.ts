5cc94c496ab34cdeee91079a916fa63a196835da
Luis Fernando <Luis Fernando Amado>
2025-04-05 12:33:21 -0500

    Se agrega header

--- apps/web/src/app/home/admin-shell/admin-shell.component.ts
+++ apps/web/src/app/home/admin-shell/admin-shell.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { AuthService, Utilidades } from '@storeflow/design-system';
+import { HeaderComponent } from '../../shared/header/header.component';
 
 @Component({
   selector: 'app-admin-shell',
   standalone: true,
-  imports: [CommonModule],
+  imports: [HeaderComponent],
   templateUrl: './admin-shell.component.html',
   styleUrl: './admin-shell.component.scss',
 })
-export class AdminShellComponent {}
+export class AdminShellComponent {
+  // quitar
+  nombreUsuario = 'Camilo Barreto';
+  authService = inject(AuthService);
+  constructor() {
+    this.authService.obtenerDatosSesion();
+  }
+
+  get sesion() {
+    return Utilidades.obtenerSesion();
+  }
+}
