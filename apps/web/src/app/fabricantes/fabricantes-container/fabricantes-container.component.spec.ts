f92f7bee350f91548393ba0ec29458a549bf1124
Luis Fernando <Luis Fernando Amado>
2025-04-11 00:30:41 -0500

    Se implementa registro de fabricante

--- apps/web/src/app/fabricantes/fabricantes-container/fabricantes-container.component.spec.ts
+++ apps/web/src/app/fabricantes/fabricantes-container/fabricantes-container.component.spec.ts
@@ -1,4 +1,6 @@
+import { provideHttpClient } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FabricantesContainerComponent } from './fabricantes-container.component';
 
 describe('FabricantesContainerComponent', () => {
@@ -7,7 +9,8 @@ describe('FabricantesContainerComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [FabricantesContainerComponent],
+      imports: [FabricantesContainerComponent, BrowserAnimationsModule],
+      providers: [provideHttpClient()],
     }).compileComponents();
 
     fixture = TestBed.createComponent(FabricantesContainerComponent);
